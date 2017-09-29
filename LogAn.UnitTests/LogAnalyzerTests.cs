59e91bbe6d1023cf6644b6acc71db8f05c6b9174
Kidd Wang <kiddkid99@gmail.com>
2017-09-29 23:50:54 +0800

    Add LogAnalyzerTests

---
+++ LogAn.UnitTests/LogAnalyzerTests.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogAn.UnitTests
+{
+    [TestFixture]
+    public class LogAnalyzerTests
+    {
+        [Test]
+        public void IsValidLogFileName_BadExtenstion_ReturnFalse()
+        {
+            LogAnalyzer analyzer = new LogAnalyzer();
+
+            bool result = analyzer.IsValidLogFileName("abc.foo");
+
+            Assert.False(result);
+        }
+    }
+}
