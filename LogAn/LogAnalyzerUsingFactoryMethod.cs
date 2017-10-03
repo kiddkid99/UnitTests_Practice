17eb5a18cc8d61379b6fe0622aa029d68cfca977
Kidd Wang <kiddkid99@gmail.com>
2017-10-03 14:58:54 +0800

    Add LogAnalyzerUsingFactoryMethod.cs

---
+++ LogAn/LogAnalyzerUsingFactoryMethod.cs
@@ -0,0 +1,15 @@
+﻿namespace LogAn
+{
+    public class LogAnalyzerUsingFactoryMethod
+    {
+        public bool IsValidLogFileName(string fileName)
+        {
+            return GetManager().IsValid(fileName);
+        }
+
+        public virtual IFileExtensionManager GetManager()
+        {
+            return new FileExtensionManager();
+        }
+    }
+}
