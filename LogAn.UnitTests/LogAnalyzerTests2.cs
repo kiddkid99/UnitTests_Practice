516ecea1d92952987ad01f8aab25b2bc9d36dd34
Kidd Wang <kiddkid99@gmail.com>
2017-10-05 22:39:18 +0800

    Add LogAnalyzerTest2 using NSubstitute

---
+++ LogAn.UnitTests/LogAnalyzerTests2.cs
@@ -0,0 +1,23 @@
+﻿using NSubstitute;
+using NUnit.Framework;
+
+namespace LogAn.UnitTests
+{
+    [TestFixture]
+    public class LogAnalyzerTests2
+    {
+        [Test]
+        public void Analyze_TooShortFileName_CallWebService()
+        {
+            IWebService service = Substitute.For<IWebService>();
+            LogAnalyzer analyzer = new LogAnalyzer();
+            analyzer.Service = service;
+
+            analyzer.MinNameLength = 6;
+            analyzer.Analyze("a.txt");
+
+            service.Received().LogError("File name too short: a.txt");
+                
+        }
+    }
+}
